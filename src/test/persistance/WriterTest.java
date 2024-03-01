3c871b5f82fd9c10141d6afdb65f75c12b6dc9fe
windy <jhuan127@students.cs.ubc.ca>
2024-02-29 21:56:08 -0800

    fixed a bug

--- src/test/persistance/WriterTest.java
+++ src/test/persistance/WriterTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import ui.Panel;
 
 import java.io.IOException;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
